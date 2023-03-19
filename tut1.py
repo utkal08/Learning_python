afdd0624692847b2ea016b077570b4697b27c9e6
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-03-19 17:13:40 +0530

    First commit

---
+++ tut1.py
@@ -0,0 +1,15 @@
+
+# message= ''
+greeting ='Hello'
+name='michael'
+
+#message=greeting+','+name+'.welcome!'
+message='{},{}.welcome!'.format(greeting,name)
+#message =""" Bobby's world was a good 
+#cartoon in the 19980s"""
+ #print(len(message ))
+#print(message [1:5])
+#print(message.count('b') )
+#print(message.replace('world','universe') )
+#print (dir(name))
+print (help(str))
