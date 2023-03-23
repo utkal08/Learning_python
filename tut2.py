f233c19a4952e5af3bb6d67a0f25ab00cf3cd424
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-03-23 21:58:53 +0530

    adding tut_2

---
+++ tut2.py
@@ -0,0 +1,24 @@
+num =3
+print(type(num))
+#Arithmetic Operators:
+#Addition: 3+2
+#subtraction: 3-2
+#multiplication: 3*2
+# floor division: 3/2
+#exponent: 3**2
+#Modulus: 3%2
+print(2+3)
+print(2*3)
+print(2-3)
+print(2/3)
+#print(abs(-3))
+print(round(3.75,1))
+num_1=3
+num_2=2
+print(num_1 !=num_2)
+print(num_1 >=num_2)
+num_3 ='100'
+num_4 ='200'
+num_3 =int(num_3)
+num_4 =int(num_4)
+print(num_3+num_4)
