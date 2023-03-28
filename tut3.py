baaf4b10be00c5964794cf2f2938040d0b9d2504
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-03-28 23:50:10 +0530

    adding tutorial 3

---
+++ tut3.py
@@ -0,0 +1,19 @@
+courses=['History','Math','Physics','chemistry']
+courses2=['Art','Education']
+#courses.append('art')# it will add a element from the last
+#courses.insert(0,'art')
+#courses.extend(courses2)
+#courses.remove('Math')
+#courses.reverse()
+num=[1,3,2,6,4]
+#num.sort()
+#courses.sort(reverse=True)
+#num.sort(reverse=True) 
+
+
+#courses.pop()
+#print(len(courses))
+#print(courses[0:2])
+#print(courses[2:])#slicing
+print(courses)
+print(num)
