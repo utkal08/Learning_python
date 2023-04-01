cb58956300ad97cef2120f6dec31c080b979ee7b
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-04-01 23:51:37 +0530

    new changes

--- tut8.py
+++ tut8.py
@@ -1,7 +1,25 @@
-def hello_func():
-    print('hello Function!')
+#def hello_func(greeting,name='You'):
+#   # print('hello Function!')
+#   return ' {}  ,{}.'.format(greeting,name)  
 
 
 #hello_func()
+#print(hello_func('hi',name='Corey'))
+#DRY
 
-print('Hello Function!')
+#print('Hello Function!')
+
+#print(len('Test'))
+
+def student_info(*args,**kwargs):
+   print(args)
+   print(kwargs)
+
+courses = {'Math','Art'}
+info ={'Name':'Johnwick','age':22}
+
+#student_info('Math','Art',name='john',age=22)
+student_info(*courses,**info)
+
+
+
