8dac5b90d304bceb7238579b9762983418d90c92
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-04-02 07:41:13 +0530

    completed fundatmental

--- tut9.py
+++ tut9.py
@@ -1,1 +1,56 @@
- 
+#import sys 
+#sys.path.append('C:\Users\utkal\Learning_python')
+#from my_module import find_index, test 
+##import my_module as mm
+#import sys
+#
+#courses =['History ','Math','Physics','CcmpSci']
+#
+#index = find_index(courses,'Math')
+##print(index)
+##print(test)
+#print(sys.path)
+
+
+#import random
+#
+#courses =['History ','Math','Physics','CompSci']
+#
+#random_course = random.choice(courses)
+#
+#print (random_course) 
+
+
+
+#import math
+#
+#courses =['History ','Math','Physics','CompSci']
+#
+#rads = math.radians(90)
+#
+#print (math.sin(rads))  
+
+#import datetime
+#import calendar
+#
+#courses =['History ','Math','Physics','CcmpSci']
+#
+#
+#today =datetime.date.today()
+#print(today)
+#
+
+
+#import os
+#
+#courses =['History ','Math','Physics','CcmpSci']
+#
+#
+#print(os.getcwd())
+#
+#print(os.__file__)
+
+
+import antigravity
+
+
