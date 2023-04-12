252074ce1d2ee12391f1336dbcf1d2360861f14c
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-04-13 00:31:03 +0530

    new changes

---
+++ pars_csv.py
@@ -0,0 +1,28 @@
+import csv
+
+html_output = ''
+names = []
+
+with open ('patrons.csv','r') as data_file:
+    csv_data = csv.reader(data_file)
+# We don't want headers or first line of bad data
+    next(csv_data)
+    next(csv_data)
+    
+
+    for line in csv_data:
+
+        for line in csv_data: 
+         if line[0] == 'No Reward' :
+            break
+        names.append(f"{line[0]} {line[1]}")
+
+html_output += f'<p> There are currently {len(names)} public contributors. Thank You!</p>'
+html_output += '\n<ul>'
+
+for name in names:
+   html_output += f'\n\t<li>{name}</li>'
+html_output += '\n</ul>'
+print(html_output)
+#for name in names:
+ #   print(name)
