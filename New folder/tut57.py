81ec514d994f4ffcdefe3eca7f47de0339bcfd33
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-09-07 23:57:53 +0530

    n

---
+++ New folder/tut57.py
@@ -0,0 +1,17 @@
+import unittest
+import calc
+
+
+class Testclac(unittest.TestCase):
+
+    def test_add(self):
+
+        result =calc.add(10,5)
+        self.assertEqual(result,15)
+
+
+if __name__ =='__main__':
+
+     unittest.main()
+
+
